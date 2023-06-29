2134b2820cad3a28b9f870a2510d7b6aed8b7c09
Ammon Knaupp <adknaupp@gmail.com>
2023-06-29 12:24:21 -0600

    Stop underlining

--- py/make_links.py
+++ py/make_links.py
@@ -9,7 +9,7 @@ for name, url in urls.items():
     url = url.replace('_', r'\_')
     # escape pound signs
     url = url.replace('#', r'\#')
-    newcommands.append(r'\newcommand{\link%s}[1]{\href{%s}{\uline{#1}}}' % (name, url))
+    newcommands.append(r'\newcommand{\link%s}[1]{\href{%s}{#1}}' % (name, url))
 
 with open('links.tex', 'w') as file:
     file.write('\n'.join(newcommands))
